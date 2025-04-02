fa20d6e9c44964462bfc326a3e6a6dd63dd0c052
David Amador <54488581+dsamador@users.noreply.github.com>
2025-04-01 21:43:18 -0500

    Barra de busqueda y filtros mejorados visualmente

--- seriales/components/list_serial.py
+++ seriales/components/list_serial.py
@@ -3,6 +3,8 @@ from seriales.models import Computador
 
 class ListSerialView(UnicornView):
     computadores: list[Computador] = None
+    query = ""
+    resultados = []
 
     def mount(self):
         self.computadores = Computador.objects.all()
@@ -15,3 +17,17 @@ class ListSerialView(UnicornView):
     
     def todos(self):
         self.computadores = Computador.objects.all()
+
+    def buscar(self):
+        if self.query:
+            letras = list(self.query)# Separa las letras
+            self.computadores = Computador.objects.filter(
+                serial__icontains=self.query  # Filtra si contiene la cadena
+            )
+            
+            # Filtra los seriales que contengan todas las letras en cualquier orden
+            self.computadores = [
+                c for c in self.computadores if all(letra in c.serial for letra in letras)
+            ]
+        else:
+            self.computadores = Computador.objects.all()
